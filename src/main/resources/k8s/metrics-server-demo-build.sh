2e05db0bd96a1a2b7f4d38888e0bebbe698d047c
高宏旭 <gaohongxu@hzsun.com>
2020-12-08 10:58:25 +0800

    build.sh文件修改

--- src/main/resources/k8s/metrics-server-demo-build.sh
+++ src/main/resources/k8s/metrics-server-demo-build.sh
@@ -10,6 +10,9 @@ JAR_PACKAGE_NAME=$(ls "${WORKSPACE}"/target | grep '.*\.jar$')
 echo "jar_package_name : ${JAR_PACKAGE_NAME}"
 
 cd "${WORKSPACE}"/target
+rm -rf Dockerfile
+rm -rf metrics-server-demo-deploy.yaml
+
 cat <<EOF > Dockerfile
 FROM openjdk:8-jre-alpine
 COPY ./${JAR_PACKAGE_NAME} /${MODULE_NAME}.jar
@@ -20,24 +23,18 @@ echo "Dockerfile created success"
 
 docker build -t "${MODULE_NAME}":"${VERSION}" .
 
-if [ 1 == 1 ];then
-    echo "退出"
-    exit 1
-fi
-
 echo "image build success"
-rm -rf Dockerfile
 
-sudo docker tag cache-demo:${version} registry.cn-hangzhou.aliyuncs.com/ghx-docker-repo/demo-cache:${version}
-sudo docker push registry.cn-hangzhou.aliyuncs.com/ghx-docker-repo/demo-cache:${version}
-docker rmi -f registry.cn-hangzhou.aliyuncs.com/ghx-docker-repo/demo-cache:${version}
+sudo docker tag "${MODULE_NAME}":"${VERSION}" registry.cn-hangzhou.aliyuncs.com/ghx-docker-repo/"${MODULE_NAME}":"${VERSION}"
+sudo docker push registry.cn-hangzhou.aliyuncs.com/ghx-docker-repo/"${MODULE_NAME}":"${VERSION}"
+docker rmi -f registry.cn-hangzhou.aliyuncs.com/ghx-docker-repo/"${MODULE_NAME}":"${VERSION}"
 
 echo "image push success"
 
-cp cache-demo-deployment-template.yaml cache-demo-deployment.yaml
-sed -i "s/{{version}}/${version}/g" cache-demo-deployment.yaml
+cp metrics-server-demo-deploy-template.yaml metrics-server-demo-deploy.yaml
+sed -i "s/{{VERSION}}/${VERSION}/g" metrics-server-demo-deploy.yaml
+sed -i "s/{{MODULE_NAME}}/${MODULE_NAME}/g" metrics-server-demo-deploy.yaml
 
-echo "$(cat cache-demo-deployment.yaml)"
+"echo $(cat cache-demo-deployment.yaml)"
 
-
-kubectl apply -f cache-demo-deployment.yaml
+kubectl apply -f metrics-server-demo-deploy.yaml
