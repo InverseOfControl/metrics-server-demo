0a2c5ebf601b51a7e5d48f18861bafb179662b6d
高宏旭 <gaohongxu@hzsun.com>
2020-12-08 11:14:49 +0800

    deploy

--- src/main/resources/k8s/metrics-server-demo-build.sh
+++ src/main/resources/k8s/metrics-server-demo-build.sh
@@ -32,6 +32,7 @@ docker rmi -f registry.cn-hangzhou.aliyuncs.com/ghx-docker-repo/"${MODULE_NAME}"
 echo "image push success"
 
 cd "${WORKSPACE}"/target/classes/k8s
+chmod +x metrics-server-demo-deploy-template.yaml
 cp metrics-server-demo-deploy-template.yaml metrics-server-demo-deploy.yaml
 sed -i "s/{{VERSION}}/${VERSION}/g" metrics-server-demo-deploy.yaml
 sed -i "s/{{MODULE_NAME}}/${MODULE_NAME}/g" metrics-server-demo-deploy.yaml
